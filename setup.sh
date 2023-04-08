890affd186ce6e67a84f51f6e0de85a003b29933
energybubu <ivanhoward011878@gmail.com>
2023-04-08 19:46:03 +0800

    remove setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@
 # Google Chrome 74.0.3729.131
 # 到chrome driver網站下載對應版本的 driver。
 # 安裝 chrome driver (v74版)
-wget https://chromedriver.storage.googleapis.com/112.0.5615.49/chromedriver_linux64.zip
+# wget https://chromedriver.storage.googleapis.com/112.0.5615.49/chromedriver_linux64.zip
 # 這邊將 chromedriver 放到家目錄下 (docker image以/root為家目錄)
-unzip chromedriver_linux64.zip -d ./
+# unzip chromedriver_linux64.zip -d ./
