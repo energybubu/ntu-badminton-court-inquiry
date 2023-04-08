cfddfb97b61d4f1d15ee0f8fe454217493f4a2ed
energybubu <ivanhoward011878@gmail.com>
2023-04-09 00:49:34 +0800

    download chrome

--- setup.sh
+++ setup.sh
@@ -1,10 +1,22 @@
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# apt install ./google-chrome-stable_current_amd64.deb
-# 查看 chrome 版本
-# google-chrome --version
-# Google Chrome 74.0.3729.131
-# 到chrome driver網站下載對應版本的 driver。
-# 安裝 chrome driver (v74版)
-# wget https://chromedriver.storage.googleapis.com/112.0.5615.49/chromedriver_linux64.zip
-# 這邊將 chromedriver 放到家目錄下 (docker image以/root為家目錄)
-# unzip chromedriver_linux64.zip -d ./
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+STORAGE_DIR=/opt/render/project/.render
+
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src # Make sure we return to where we were
+else
+  echo "...Using Chrome from cache"
+fi
+
+# be sure to add Chromes location to the PATH as part of your Start Command
+export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
+
+# add your own build commands...
