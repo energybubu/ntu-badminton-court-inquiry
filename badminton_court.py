a8ceee4f3ef23ec48844ede916dc21232da7a75b
energybubu <ivanhoward011878@gmail.com>
2023-04-09 03:12:44 +0800

    auto scroll

--- badminton_court.py
+++ badminton_court.py
@@ -34,11 +34,11 @@ class Page:
 def update_csvs(username=os.environ['USER'], password=os.environ['PASSWORD']):
 
     options = Options()
-    # options.add_argument('--headless')
-    # options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
     options.add_argument("--window-size=1920, 1080")
-    driver = webdriver.Chrome(options=options)
-    # driver = webdriver.Chrome('./chromedriver', chrome_options=options)
+    # driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome('./chromedriver', chrome_options=options)
 
     # opts = webdriver.ChromeOptions()
     # opts.add_argument('--headless')
