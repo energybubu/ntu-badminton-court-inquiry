c6d2c50d5d98f024973568742d5c374a4e5304a0
energybubu <ivanhoward011878@gmail.com>
2023-04-08 16:02:13 +0800

    Line bot feature

--- badminton_court.py
+++ badminton_court.py
@@ -3,108 +3,186 @@
 from bs4 import BeautifulSoup as BS
 from selenium import webdriver
 import time 
-import sys
+import os, sys
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from tqdm import tqdm
-
-driver = webdriver.Chrome("./chromedriver")
+import requests
+import pandas as pd
+from datetime import datetime
 class Page:
-    def __init__(self, url):
+
+    def __init__(self, url, driver):
         self.url = url
-        driver.get(url)
+        self.driver = driver
+        self.driver.get(url)
 
     def pass_alert(self):
-        WebDriverWait(driver, 10).until(EC.alert_is_present())
-        driver.switch_to.alert.accept()
+        WebDriverWait(self.driver, 10).until(EC.alert_is_present())
+        self.driver.switch_to.alert.accept()
     def click_button(self, selector="id", info=""):
-        button = driver.find_element(selector, info)
+        button = self.driver.find_element(selector, info)
         button.click()
     def enter_text(self, selector, info, text):
-        element = driver.find_element(selector, info)
+        element = self.driver.find_element(selector, info)
         element.send_keys(text)
-url = "https://rent.pe.ntu.edu.tw/member/?U=login"
+def update_csvs(username=os.environ['USER'], password=os.environ['PASSWORD']):
+    opts = webdriver.ChromeOptions()
+    opts.add_argument('--headless')
+    driver = webdriver.Chrome(options=opts)
+    url = "https://rent.pe.ntu.edu.tw/member/?U=login"
+    new_page = Page(url, driver)
+    new_page.click_button("css selector", "a.LoginBtn")
+    new_page.enter_text("id", 'ContentPlaceHolder1_UsernameTextBox', username)
+    new_page.enter_text("id", 'ContentPlaceHolder1_PasswordTextBox', password)
+    new_page.click_button("id", "ContentPlaceHolder1_SubmitButton")
+    new_page.pass_alert()
+    new_page.click_button("xpath", '//a[@href="'+"/order/?Add=A:2"+'"]')
+    driver.execute_script("window.scrollTo(0, 600)")
 
-new_page = Page(url)
-new_page.click_button("css selector", "a.LoginBtn")
-new_page.enter_text("id", 'ContentPlaceHolder1_UsernameTextBox', sys.argv[1])
-new_page.enter_text("id", 'ContentPlaceHolder1_PasswordTextBox', sys.argv[2])
-new_page.click_button("id", "ContentPlaceHolder1_SubmitButton")
-new_page.pass_alert()
-new_page.click_button("xpath", '//a[@href="'+"/order/?Add=A:2"+'"]')
-driver.execute_script("window.scrollTo(0, 200)")
+    DATE = {0:"日", 1:"一", 2:"二", 3:"三", 4:"四", 5:"五", 6:"六"}
 
-DATE = {0:"日", 1:"一", 2:"二", 3:"三", 4:"四", 5:"五", 6:"六"}
+    courts = driver.find_elements("class name", "VSMenu")
+    soup_col = []
+    for court in tqdm(courts):
+        suc = 0
+        while suc!=1:
+            try:
+                court.click()
+                time.sleep(0.5)
+                soup_col.append(BS(driver.page_source, "lxml"))
+                suc = 1
+            except:
+                print("Please scroll your screen to see '一面, 二面.....'")
+                pass
 
-courts = driver.find_elements("class name", "VSMenu")
-soup_col = []
-for court in tqdm(courts):
-    suc = 0
-    while suc!=1:
+    import os 
+    if not os.path.isdir("./court_information"): os.mkdir("./court_information")
+    if not os.path.isdir("./summary"): os.mkdir("./summary")
+    court_path = "./court_information"
+    summary_path = "./summary"
+
+
+    import numpy as np
+    online = np.zeros([14, 21])
+    offline = np.zeros([14, 21])
+    dates = []
+    index = [f"{i}-{i+1}" for i in range(8, 22)]
+
+    '''靜態網頁擷取'''
+    for numm, soup in enumerate(soup_col):
+        table = soup.find("div", {"class":"SContents"})
+
+        with open(os.path.join(court_path, f"court{numm+1}.txt"), "w") as file:
+            file.write(f"這是第{numm+1}面\n")
+            
+            days = table.find_all('div', {"class":"D"})
+            '''可以網上租借的時段'''
+            file.write("可以網上租借\n")
+            for num, day in enumerate(days):
+                availables = day.find_all('a', {"class":"S Hover"})
+                # if numm==0: dates.append(f"{day['d']} ({DATE[num%7]})")
+                if numm==0: dates.append(f"{day['d']}")
+                if availables:
+                    file.write(str(day['d'])+" "+str(DATE[num%7])+"\n")
+                for available in availables:
+                    file.write(str(available['title'])+"\n")
+                    online[int(available['title'].split(' ')[0])-8][num] += 1
+
+            '''可以現場租借的時段'''
+            file.write("請至現場租借\n")
+            for num, day in enumerate(days):
+                all_possible = day.find_all('div', {'s':'N'})
+                print_day = 0
+                for possible in all_possible:
+                    if len(possible.contents)>=2:
+                        if possible.contents[1]['class'][1] == "N4":
+                            if print_day==0:
+                                file.write(str(day['d'])+" "+str(DATE[num%7])+"\n")
+                                print_day = 1
+                            file.write(possible.contents[0].get_text()+"\n")
+                            offline[int(possible.contents[0].get_text().split()[0])-8][num] += 1
+
+    online_pd = pd.DataFrame(online, columns=dates, index=index, dtype='int64')
+    online_pd = online_pd.loc[:, (online_pd != 0).any(axis=0)]
+    offline_pd = pd.DataFrame(offline, columns=dates, index=index, dtype='int64')
+    offline_pd = offline_pd.loc[:, (offline_pd != 0).any(axis=0)]
+    online_pd.to_csv(os.path.join(summary_path, "可線上租借剩餘場數.csv"))
+    offline_pd.to_csv(os.path.join(summary_path, "可現場租借剩餘場數.csv"))
+
+    ret_msg = f"{'='*10}\n可線上租借剩餘場數\n{'='*10}\n{online_pd}\n{'='*10}可現場租借剩餘場數{'='*10}\n{offline_pd}"
+def Query_all():
+    update_csvs()
+    online_pd = pd.read_csv("./summary/可線上租借剩餘場數.csv")
+    offline_pd = pd.read_csv("./summary/可現場租借剩餘場數.csv")
+    week_list = ["星期一","星期二","星期三","星期四","星期五","星期六","星期日"]
+
+    ret_col = []
+    ret_col.append("=== 可線上租借剩餘場數 ===\n")
+    for column in online_pd.columns.values[1:]:
+        ret = f"{column} "
+        weekday = datetime.strptime(column, '%Y-%m-%d').weekday()
+        ret += f"{week_list[weekday]}\n"
+        for date, court in zip(online_pd['Unnamed: 0'].values, online_pd[column].values):
+            ret += date.ljust(8)
+            ret += str(court).rjust(4)
+            ret += '\n'
+        ret_col.append(ret)
+    ret_col.append("=== 可現場租借剩餘場數 ===\n")
+    for column in offline_pd.columns.values[1:]:
+        ret = f"{column} "
+        weekday = datetime.strptime(column, '%Y-%m-%d').weekday()
+        ret += f"{week_list[weekday]}\n"
+        for date, court in zip(offline_pd['Unnamed: 0'].values, offline_pd[column].values):
+            ret += date.ljust(8)
+            ret += str(court).rjust(4)
+            ret += '\n'
+        ret_col.append(ret)
+    return ret_col
+def Query(username=os.environ['USER'], password=os.environ['PASSWORD'], assign_date="2023-04-11"):
+    # try:
+    #     online_pd = pd.read_csv("./summary/可線上租借剩餘場數.csv")
+    #     offline_pd = pd.read_csv("./summary/可現場租借剩餘場數.csv")
+    #     ret = ""
+    #     for date, courts in zip(online_pd['Unnamed: 0'].values, online_pd[assign_date].values):
+    #         ret += date.ljust(8)
+    #         ret += str(courts).rjust(4)
+    #         ret += '\n'
+    #     return ret
+    # except:
+    #     try:
+    #         ret = ""
+    #         for date, courts in zip(offline_pd['Unnamed: 0'].values, offline_pd[assign_date].values):
+    #             ret += date.ljust(8)
+    #             ret += str(courts).rjust(4)
+    #             ret += '\n'
+    #         return ret
+    #     except:
+    #         pass
+    update_csvs(username=os.environ['USER'], password=os.environ['PASSWORD'])
+    try:
+        online_pd = pd.read_csv("./summary/可線上租借剩餘場數.csv")
+        offline_pd = pd.read_csv("./summary/可現場租借剩餘場數.csv")
+        ret = ""
+        for date, courts in zip(online_pd['Unnamed: 0'].values, online_pd[assign_date].values):
+            ret += date.ljust(8)
+            ret += str(courts).rjust(4)
+            ret += '\n'
+        return ret
+    except:
         try:
-            court.click()
-            time.sleep(0.5)
-            soup_col.append(BS(driver.page_source, "lxml"))
-            suc = 1
+            ret = ""
+            for date, courts in zip(offline_pd['Unnamed: 0'].values, offline_pd[assign_date].values):
+                ret += date.ljust(8)
+                ret += str(courts).rjust(4)
+                ret += '\n'
+            return ret
         except:
-            print("Please scroll your screen to see '一面, 二面.....'")
-            pass
-
-import os 
-if not os.path.isdir("./court_information"): os.mkdir("./court_information")
-if not os.path.isdir("./summary"): os.mkdir("./summary")
-court_path = "./court_information"
-summary_path = "./summary"
+            return "請輸入一週內的日期，不好意思。\n\n如果需要占卜的功能，歡迎私訊小編，可以付費解鎖！"
 
 
-import numpy as np
-online = np.zeros([14, 21])
-offline = np.zeros([14, 21])
-dates = []
-index = [f"{i}-{i+1}" for i in range(8, 22)]
-
-'''靜態網頁擷取'''
-for numm, soup in enumerate(soup_col):
-    table = soup.find("div", {"class":"SContents"})
-
-    with open(os.path.join(court_path, f"court{numm+1}.txt"), "w") as file:
-        file.write(f"這是第{numm+1}面\n")
-        
-        days = table.find_all('div', {"class":"D"})
-        '''可以網上租借的時段'''
-        file.write("可以網上租借\n")
-        for num, day in enumerate(days):
-            availables = day.find_all('a', {"class":"S Hover"})
-            if numm==0: dates.append(f"{day['d']} ({DATE[num%7]})")
-            if availables:
-                file.write(str(day['d'])+" "+str(DATE[num%7])+"\n")
-            for available in availables:
-                file.write(str(available['title'])+"\n")
-                online[int(available['title'].split(' ')[0])-8][num] += 1
-
-        '''可以現場租借的時段'''
-        file.write("請至現場租借\n")
-        for num, day in enumerate(days):
-            all_possible = day.find_all('div', {'s':'N'})
-            print_day = 0
-            for possible in all_possible:
-                if len(possible.contents)>=2:
-                    if possible.contents[1]['class'][1] == "N4":
-                        if print_day==0:
-                            file.write(str(day['d'])+" "+str(DATE[num%7])+"\n")
-                            print_day = 1
-                        file.write(possible.contents[0].get_text()+"\n")
-                        offline[int(possible.contents[0].get_text().split()[0])-8][num] += 1
-
-import pandas as pd
-online_pd = pd.DataFrame(online, columns=dates, index=index, dtype='int64')
-online_pd = online_pd.loc[:, (online_pd != 0).any(axis=0)]
-offline_pd = pd.DataFrame(offline, columns=dates, index=index, dtype='int64')
-offline_pd = offline_pd.loc[:, (offline_pd != 0).any(axis=0)]
-print("="*10, "可線上租借剩餘場數", "="*10)
-print(online_pd)
-print("="*10, "可現場租借剩餘場數", "="*10)
-print(offline_pd)
-online_pd.to_csv(os.path.join(summary_path, "可線上租借剩餘場數.csv"))
-offline_pd.to_csv(os.path.join(summary_path, "可現場租借剩餘場數.csv"))
+if __name__ == "__main__":
+    msg = Query(username=os.environ['USER'], password=os.environ['PASSWORD'], assign_date="2023-04-11")
+    msg = Query_all()
+    print(msg)
---
+++ line.py
@@ -0,0 +1,124 @@
+from flask import Flask, request, abort
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from badminton_court import Query, Query_all
+
+from pprint import pprint
+
+
+import openai, os
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('yZwUTzxEf17CtCIgR12stNgdFEd/WsiXEV9E+DLUHH9UDQ4S5iWt9+5Dh6CkMI0fTErnU3ve0IuEPPgBUC2fgIrp7+GeeFiugj6a31Kp+rPyQjOcZSemyo9OcUCbRQOxYgxg79Vb+jmW9/bWSwJjIAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('8e5bdf4d1c4e88d5f9a2428bac79712d')
+def chat(text):
+    ''' create your own api key on openai '''
+    openai.api_key = "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"
+    response = openai.Completion.create(
+        engine = "text-davinci-003", # Choose the best model
+        prompt = "What is predicate?", # Enter your question
+        max_tokens = 30,
+        temperature=0.9,
+        top_p=0.75,
+        n=1
+    )
+    return response["choices"][0]['text']
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    # print("Request body: " + body, "Signature: " + signature)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+import datetime
+from datetime import date
+import requests
+def notify(event, user):
+
+    headers = {
+        "Authorization": "Bearer " + "F5amfMBnKpxQVcyMa6Q1ZN0dMdnYg5bdmscRHQpbLNj",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+ 
+    params = {"message": f"{user}: {event.message.text}"}
+ 
+    r = requests.post("https://notify-api.line.me/api/notify",
+                      headers=headers, params=params)
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    try:  
+        profile = line_bot_api.get_profile(event.source.user_id)
+        user = profile.display_name
+        if profile.display_name!="李沅錡": notify(event, user)
+        if profile.display_name=="李沅錡":
+            print(profile.display_name, "正在節省他的時間")
+        elif profile.display_name=="樂":
+            print("樂正在使用")
+            line_bot_api.push_message(
+                event.source.user_id,
+                TextSendMessage(text="寶貝！歡迎使用我的機器人！\nI love you ~~"))
+        elif profile.display_name=="涂宇杰":
+            print("涂宇杰正在使用, 一次50")
+            line_bot_api.push_message(
+                event.source.user_id,
+                TextSendMessage(text="幹嘛，我有說要給你用嗎？\n一次50，月結。"))
+        else:
+            print(profile.display_name, "正在偷用你的機器人")
+    except :
+        print("Failed to Notify")
+    print("Text:", event.message.text)
+    content = "{}: {}".format(event.source.user_id, event.message.text)
+    query_date = event.message.text.replace(" ", "").replace("\n", "")
+    if query_date=="all" or query_date=="All": 
+        arr = Query_all()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='\n'.join(arr)))
+    else:
+        try:
+            todate = datetime.datetime.today()
+            print("todate:", todate)
+            q_date = datetime.datetime.strptime(query_date, '%Y-%m-%d')
+            print("q_date:", q_date)
+            dif = (q_date-todate).days
+            print("dif: ", dif)
+            if dif<-1:
+                content = "請輸入一週內的日期。\n\n人要往前看，不要再活在過去了！！"
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=content))
+            elif dif>7:
+                content = "請輸入一週內的日期。\n\n如果需要占卜的功能，歡迎私訊小編，可以付費解鎖！"
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=content))
+            else:
+                query_date = q_date.strftime('%Y-%m-%d')
+                content = Query(username=os.environ['USER'], password=os.environ['PASSWORD'], assign_date=query_date)
+
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=content))
+        except:
+            text = f"你仔細看看自己打了什麼\n*** ?? ***\n{event.message.text}\n*** ?? ***\n\n請按照以下格式輸入\n\n1.YYYY-MM-DD\n查詢特定日期剩餘場次\n\n2.All/all\n查詢一週內剩餘場次"
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=text))
+            
+
+
+import os
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=os.environ['PORT'])
